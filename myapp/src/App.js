e882adb2454c00a27e5b0bfacbc2a7389fffea01
amoghnalge <amoghnalge@gmail.com>
2021-07-29 10:26:48 +0530

    first commit

---
+++ myapp/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  // let name = "Amogh";
+  // let list = ["mumbai", "delhi", "kolkata", "chennai"];
+  let title = "onclick demo";
+  let myfun = () => {
+    alert("hello");
+  };
+  return (
+    <div>
+      <h1>{title}</h1>
+      <input type="button" value="click" onClick={myfun} />
+    </div>
+  );
+}
+
+export default App;
